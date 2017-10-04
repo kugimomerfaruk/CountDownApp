ad0c87dd6a26db4df5e5a0a51ebb78cc5e00ffe1
ST1 <st1@ST1-iMac.local>
2017-10-04 00:41:53 -0700

    First Commit

--- FocusCountDown/ViewController.swift
+++ FocusCountDown/ViewController.swift
@@ -9,15 +9,77 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var second = 5
+    var timer = Timer()
+    
+    @IBOutlet weak var countDownLabel: UILabel!
+    @IBOutlet var progressBarOutlet: UIProgressView!
+    @IBOutlet weak var stepperOutlet: UIStepper!
+    @IBOutlet weak var actionViewOutlet: UIView!
+    
+    
+    
+    @IBAction func stepperAction(_ sender: UIStepper) {
+        
+    countDownLabel.text = String(Int(sender.value))
+        print(sender.value)
+        //progressBarOutlet.progress = 0.0
+        
+        
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //print("Ekrana Dokundun")
+        actionViewOutlet.isHidden = true
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.timerFunc)), userInfo: nil, repeats: true)
+        
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //print("Elini Çektin")
+        timer.invalidate()
+        actionViewOutlet.isHidden = false
+        
+    }
+    func timerFunc() {
+        print("Çalışıyor")
+        if second <= 0 {
+        timer.invalidate()
+            second = 0
+            print("Harika Tamamladınız")
+            return
+            //timer Durdur
+            
+            
+            
+        }
+        second -= 1
+        
+        print(second)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
